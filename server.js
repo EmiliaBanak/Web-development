b0fc97b9199725b7470507445d2e66024fc6b7a7
adve23wf <adve23wf@student.ju.se>
2026-03-04 11:33:37 +0100

    Start of backend
    Installed express, cors and connected to local host

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+app.use(express.json());
+const PORT = 3000;
+
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
