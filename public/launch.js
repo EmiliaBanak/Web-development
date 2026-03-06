0315598f3da69eec57130e3fbafcf07a78a8d7f5
adve23wf <adve23wf@student.ju.se>
2026-03-06 13:15:19 +0100

    Adding data to db

--- public/launch.js
+++ public/launch.js
@@ -2,3 +2,21 @@ if (!localStorage.getItem("visited")) {
   alert("Welcome to Jönköping City!");
   localStorage.setItem("visited", "yes");
 }
+
+fetch("http://localhost:3000/venues")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not OK");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log("GET response data:", data);
+    const list = document.getElementById("shopsList");
+
+    data.forEach((country) => {
+      const li = document.createElement("li");
+      li.textContent = country.name;
+      list.appendChild(li);
+    });
+  });
--- server.js
+++ server.js
@@ -4,11 +4,24 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 const PORT = 3000;
+//const client = require("db");
+//const { client } = require("db.js");
 
 app.use(express.static("public"));
 
+//Venues
+/*
+app.get("/venues", async (req, res) => {
+ /* try {
+    const dbres = await client.query("SELECT * FROM stores;");
+    console.log("Stores:", dbres.rows);
+    res.json(dbres.rows);
+  } catch (err) {
+    console.error("Error selecting records", err.stack);
+  }
+  console.log("hello");
+});
+*/
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
-
-//Venues
